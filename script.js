e390b708f053d099b1d6c041aa0d31808bb19149
cara.storath <cara.storath@amac02cl21xlvdl.fritz.box>
2020-11-26 00:41:01 +0100

    reset width

--- script.js
+++ script.js
@@ -100,10 +100,13 @@ function resetBoard(){
    [firstCard,secondCard] = [null,null];
 }
 
+
+
 (function shuffle(){
     cards.forEach(card => {
         let randomPos = Math.floor(Math.random()* 12);
         card.style.order = randomPos;
+
     }); 
 })();
 
